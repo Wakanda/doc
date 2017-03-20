42d32841c0969fdf639c2bd66169c464e61ab5ba
Hamza HIKMAT <hamzahik@gmail.com>
2017-03-20 19:38:33 +0100

    indentation

--- web/src/app/tutorial-detail-customized/tutorial-detail-customized.component.ts
+++ web/src/app/tutorial-detail-customized/tutorial-detail-customized.component.ts
@@ -3,63 +3,50 @@ import { TutorialElement } from '../tutorial-element';
 import { ELEMENTS } from '../mock-tutorial-data';
 
 @Component({
-  selector: 'app-tutorial-detail-customized',
-  templateUrl: './tutorial-detail-customized.component.html',
-  styleUrls: ['./tutorial-detail-customized.component.css']
+	selector: 'app-tutorial-detail-customized',
+	templateUrl: './tutorial-detail-customized.component.html',
+	styleUrls: ['./tutorial-detail-customized.component.css']
 })
 export class TutorialDetailCustomizedComponent implements OnInit {
 
-  content :string;
+	content :string;  
+	tutorial : TutorialElement;   
   
-  tutorial : TutorialElement;
-    
-  elements = ELEMENTS;
-    
-  datastores =this.elements.datastores;    
+	elements   = ELEMENTS;    
+	datastores = this.elements.datastores;  
+	fronts     = this.elements.frontTechnology; 
+	mobs       = this.elements.mobileTechnology;  
+	sessions   = this.elements.session;  
+	submitted  = false;  
+	types      = this.elements.type;
   
-  fronts  = this.elements.frontTechnology;    
- 
-  mobs  = this.elements.mobileTechnology;    
-  
-  sessions  = this.elements.session; 
-  
-  submitted = false;
-  
-  types = this.elements.type;
-  
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {  
-     
-     this.initForm();
-      
-  }
+	ngOnInit() {     
+		this.initForm();      
+	}
   
-   initForm() {
-   	  
-   	  this.tutorial = {
-          type :  this.types[1],
-          datastores : [],
-          frontTechnology : this.fronts[1].value,
-          mobileTechnology : this.mobs[1].value,
-          session : this.sessions[0].value,
-      }
-   }
+	initForm() {   	  
+		this.tutorial = {
+			type :  this.types[1],
+			datastores : [],
+			frontTechnology : this.fronts[1].value,
+			mobileTechnology : this.mobs[1].value,
+			session : this.sessions[0].value,
+		};
+	}
    
-   onSubmit(tutoForm:TutorialElement){
-   		
-   		this.submitted = true;
-   }
+	onSubmit(tutoForm:TutorialElement){   		
+		this.submitted = true;
+	}
    
+	back(){
+		this.initForm();
+		this.submitted = false;
+		this.content = "";
+	}
    
-   back(){
-   		this.initForm();
-        this.submitted = false;
-        this.content="";
-   }
-   
-   
-   getContent(tuto){
-       this.content = tuto;
-   }
+	getContent(tuto){
+		this.content = tuto;
+	}
 }

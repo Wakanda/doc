3c89b1438a6bec05a99a43db86c118b5c97998ad
Hamza HIKMAT <hamzahik@gmail.com>
2017-03-20 19:40:47 +0100

    Guide : make the menu dynamic and load content from html files

--- web/src/app/guide/guide.component.ts
+++ web/src/app/guide/guide.component.ts
@@ -1,20 +1,99 @@
 import { Component, OnInit } from '@angular/core';
+import GuideData from './guide.data';
 
 @Component({
-  selector: 'app-guide',
-  templateUrl: './guide.component.html',
-  styleUrls: ['./guide.component.css']
+	selector: 'app-guide', 
+	templateUrl: './guide.component.html',
+	styleUrls: ['./guide.component.css']
 })
 export class GuideComponent implements OnInit {
-
- content : string;
+	
+	currentSection : string;
+	
+	atRoot: boolean;
+	
+	title : string;
+	
+	backSection : any;
+	
+	backTitle : string;
+	
+	menu : any;
+	
+	sections : any;
+	
+	content : string;
  
-  constructor() {}
+	constructor() {}
 
-  ngOnInit() {}
-
+	ngOnInit() {
+		this.menu = GuideData.tree;
+		
+		this.displaySection(this.menu);
+	}
   
-  getContent(content){
-  	this.content = content;
-  }
+	displaySection(section){
+		
+		if(!section){
+			section = this.menu
+		}
+		
+		
+		this.currentSection = section.id;
+		
+		if(section.sections){
+			this.atRoot   = section.root;
+			this.sections = section.sections;
+			this.title    = section.title;
+		}
+		
+		this.setBackSection(section);
+		
+		this.displayPage(section.path);		
+	}
+	
+	displayPage(path){
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', `../assets/sections/${path}`);
+		xhr.send();
+		
+		xhr.onload = ()=>{
+			this.content = xhr.responseText;
+		};
+	}
+	
+	goBack(){
+		this.displaySection(this.backSection);
+	}
+	
+	setBackSection(section){
+		let IDs = section.id.split('-');
+		let backSection = null;
+		let sections = this.menu;
+		
+		if(IDs.length = 1){
+			this.backSection = this.menu;
+			return;
+		}
+		
+		for(let i = 0; i<IDs.length-1; i++){
+			backSection = this.getSubSectionByID(sections, IDs.slice(0,i+1).join('-'));
+		}
+		
+		this.backSection = backSection;
+	}
+	
+	getSubSectionByID(sections, id){
+		let section = null;
+		
+		sections.some(_section=>{
+			if(_section.id === id){
+				section = _section;
+				return true;
+			}
+			return false;
+		});
+		
+		return section;
+	}
 }

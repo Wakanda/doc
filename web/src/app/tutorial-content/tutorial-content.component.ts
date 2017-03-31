d3a3672b36d003b01dca09742aa7bd34301a7902
Intissar Mez <intissar.elmezrouai@wakanda.io>
2017-03-31 10:43:28 +0100

    Add content of built in db and local session

--- web/src/app/tutorial-content/tutorial-content.component.ts
+++ web/src/app/tutorial-content/tutorial-content.component.ts
@@ -1,1 +1,71 @@
-import { Component, OnInit,Input } from '@angular/core';@Component({  selector: 'app-tutorial-content',  templateUrl: './tutorial-content.component.html',  styleUrls: ['./tutorial-content.component.css']})export class TutorialContentComponent implements OnInit {      @Input()  content;     constructor() {}ngOnInit() {}}
+import { Component, OnInit, Input, Compiler, ViewContainerRef, NgModule } from '@angular/core';
+import { PartsModule } from '../parts/parts.module';
+@Component({
+  selector: 'app-tutorial-content',
+  templateUrl: './tutorial-content.component.html',
+  styleUrls: ['./tutorial-content.component.css']
+})
+export class TutorialContentComponent implements OnInit {
+    
+  @Input()
+  content;
+  
+ 
+  constructor(
+  	private compiler: Compiler,
+  	private container: ViewContainerRef
+  ) { }
+
+ ngOnChanges() {
+  	if(!this.content){
+  		return;
+  	}
+  
+  	this.content = `
+  	<md-card style='box-shadow:none'>
+		<md-card-content>
+			${this.content}
+		</md-card-content>
+	</md-card>`;
+  	
+  	let type   = this.createNewComponent(this.content);
+    let module = this.createComponentModule(type);
+    
+  	const compiled = this.compiler.compileModuleAndAllComponentsSync(module);
+  	
+  	const factory = compiled.componentFactories.filter(factory=>{
+  		return factory.selector === 'dynamic-component';
+  	})[0];
+    this.container.clear();    
+    this.container.createComponent(factory);
+  }
+ngOnInit() {}
+  createNewComponent(tmpl:string){
+  	@Component({
+      selector: 'dynamic-component',
+      template: tmpl,
+    })
+    class CustomDynamicComponent {
+      @Input()  public entity: any;
+    };
+    // a component for this particular template
+    return CustomDynamicComponent;
+  }
+  
+  createComponentModule(componentType: any){
+  	@NgModule({
+        imports: [
+        	PartsModule
+        ],
+        declarations: [
+          componentType
+        ],
+      })
+      class RuntimeComponentModule
+      {
+      }
+    // a module for just this Type
+    return RuntimeComponentModule;
+  }
+
+}

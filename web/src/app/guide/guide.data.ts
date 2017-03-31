d9dcb4968942fe407a187c509f24dbe107d06810
Yann <yann.jego@wakanda.io>
2017-03-31 14:42:01 +0200

    Add click home button + Fix back button

--- web/src/app/guide/guide.data.ts
+++ web/src/app/guide/guide.data.ts
@@ -1,179 +1,1 @@
-export default {
-    tree: {
-        "id": "main",
-        "title": "",
-        "root": true,
-        "path": "home.html",
-        "sections": [{
-                "id": "main-overview",
-                "title": "Overview",
-                "path": "overview.html",
-                "sections": [{
-                        "id": "main-overview-solution",
-                        "title": "Solution",
-                        "path": "overview/solution.html"
-                    },
-                    {
-                        "id": "main-overview-project",
-                        "title": "Project",
-                        "path": "overview/project.html"
-                    },
-                    {
-                        "id": "main-overview-bootstrap",
-                        "title": "Bootstrap",
-                        "path": "overview/bootstrap.html"
-                    },
-                    {
-                        "id": "main-overview-modules",
-                        "title": "Modules",
-                        "path": "overview/modules.html"
-                    },
-                    {
-                        "id": "main-overview-services",
-                        "title": "Services",
-                        "path": "overview/services.html"
-                    }
-                ]
-            },
-            {
-                "title": "Data Integration",
-                "path": "data.html",
-                "id": "main-data",
-                "sections": [{
-                        "title": "Basics",
-                        "path": "data/basics.html",
-                        "id": "main-data-basics"
-                    },
-                    {
-                        "title": "Client side",
-                        "path": "data/client-side.html",
-                        "id": "main-data-cs",
-                        "sections": [{
-			              "title": "Instantiate",
-			              "id": "main-data-cs-instance",
-			              "path": "data/client-side/javascript-client-instance.html"
-			            },
-			            {
-			              "title": "Get catalog",
-			              "id": "main-data-cs-catalog",
-			              "path": "data/client-side/javascript-client-get-catalog.html"
-			            },
-			            {
-			              "title": "Retrieve entities",
-			              "id": "main-data-cs-retrieve",
-			              "path": "data/client-side/javascript-client-retrieve-entities.html"
-			            },
-			            {
-			              "title": "Manipulate entity",
-			              "id": "main-data-cs-manipulate",
-			              "path": "data/client-side/javascript-client-manipulate-entity.html"
-			            },
-			            {
-			              "title": "Related entities",
-			              "id": "main-data-cs-related",
-			              "path": "data/client-side/javascript-client-related-entities.html"
-			            },
-			            {
-			              "title": "Upload",
-			              "id": "main-data-cs-upload",
-			              "path": "data/client-side/javascript-client-upload.html"
-			            },
-			            {
-			              "title": "Collection helpers",
-			              "id": "main-data-cs-helpers",
-			              "path": "data/client-side/javascript-client-collection-helpers.html"
-			            },
-			            {
-			              "title": "Server methods",
-			              "id": "main-data-cs-servermethods",
-			              "path": "data/client-side/javascript-client-server-methods.html"
-			            },
-			            {
-			              "title": "Directory methods",
-			              "id": "main-data-cs-directorymethods",
-			              "path": "data/client-side/javascript-client-directory-methods.html"
-			            },
-			            {
-			              "title": "Base services",
-			              "id": "main-data-cs-baseservices",
-			              "path": "data/client-side/javascript-client-base-services.html"
-			            }
-	            	]
-                    },
-                    {
-                        "title": "Server Side",
-                        "path": "data/server-side.html",
-                        "id": "main-data-ss"
-                    },
-                    {
-                        "title": "Security",
-                        "path": "data/security.html",
-                        "id": "main-data-security"
-                    },
-                    {
-                        "title": "Adding external data sources",
-                        "path": "data/adding-data-sources.html",
-                        "id": "main-data-external"
-                    }
-                ]
-            },
-            {
-                "title": "Authentication",
-                "path": "authentication.html",
-                "id": "main-auth",
-                "sections": [{
-                        "title": "Default Mechanism",
-                        "path": "authentication/default.html",
-                        "id": "main-auth-default"
-                    },
-                    {
-                        "title": "Custom Authentication",
-                        "path": "authentication/custom.html",
-                        "id": "main-auth-custom"
-                    },
-                    {
-                        "title": "Custom Session Management",
-                        "path": "authentication/custom-session-management.html",
-                        "id": "main-auth-csm"
-                    }
-                ]
-            },
-            {
-                "id": "main-http",
-                "title": "HTTP",
-                "path": "http.html"
-            },
-            {
-                "id": "main-workers",
-                "title": "Workers",
-                "path": "threads.html",
-                "sections": [{
-		              "title": "System Worker",
-		              "id": "main-workers-system",
-		              "path": "threads/system-worker.html"
-		            },
-		            {
-		              "title": "Shared Worker",
-		              "id": "main-workers-shared",
-		              "path": "threads/shared-worker.html"
-		            },
-		            {
-		              "title": "Node Worker",
-		              "id": "main-workers-node",
-		              "path": "threads/node-worker.html"
-		            }
-            	]
-            },
-            {
-                "id": "main-admin",
-                "title": "Administration",
-                "path": "administration.html"
-            },
-            {
-                "id": "main-misc",
-                "title": "Miscellaneous",
-                "path": "miscellaneous.html"
-            }
-        ]
-    }
-};
+export default {    tree: {        "id": "main",        "title": "Home",        "root": true,        "path": "home.html",        "sections": [{                "id": "main-overview",                "title": "Overview",                "path": "overview.html",                "sections": [{                        "id": "main-overview-solution",                        "title": "Solution",                        "path": "overview/solution.html"                    },                    {                        "id": "main-overview-project",                        "title": "Project",                        "path": "overview/project.html"                    },                    {                        "id": "main-overview-bootstrap",                        "title": "Bootstrap",                        "path": "overview/bootstrap.html"                    },                    {                        "id": "main-overview-modules",                        "title": "Modules",                        "path": "overview/modules.html"                    },                    {                        "id": "main-overview-services",                        "title": "Services",                        "path": "overview/services.html"                    }                ]            },            {                "title": "Data Integration",                "path": "data.html",                "id": "main-data",                "sections": [{                        "title": "Basics",                        "path": "data/basics.html",                        "id": "main-data-basics"                    },                    {                        "title": "Client side",                        "path": "data/client-side.html",                        "id": "main-data-cs",                        "sections": [{			              "title": "Instantiate",			              "id": "main-data-cs-instance",			              "path": "data/client-side/javascript-client-instance.html"			            },			            {			              "title": "Get catalog",			              "id": "main-data-cs-catalog",			              "path": "data/client-side/javascript-client-get-catalog.html"			            },			            {			              "title": "Retrieve entities",			              "id": "main-data-cs-retrieve",			              "path": "data/client-side/javascript-client-retrieve-entities.html"			            },			            {			              "title": "Manipulate entity",			              "id": "main-data-cs-manipulate",			              "path": "data/client-side/javascript-client-manipulate-entity.html"			            },			            {			              "title": "Related entities",			              "id": "main-data-cs-related",			              "path": "data/client-side/javascript-client-related-entities.html"			            },			            {			              "title": "Upload",			              "id": "main-data-cs-upload",			              "path": "data/client-side/javascript-client-upload.html"			            },			            {			              "title": "Collection helpers",			              "id": "main-data-cs-helpers",			              "path": "data/client-side/javascript-client-collection-helpers.html"			            },			            {			              "title": "Server methods",			              "id": "main-data-cs-servermethods",			              "path": "data/client-side/javascript-client-server-methods.html"			            },			            {			              "title": "Directory methods",			              "id": "main-data-cs-directorymethods",			              "path": "data/client-side/javascript-client-directory-methods.html"			            },			            {			              "title": "Base services",			              "id": "main-data-cs-baseservices",			              "path": "data/client-side/javascript-client-base-services.html"			            }	            	]                    },                    {                        "title": "Server Side",                        "path": "data/server-side.html",                        "id": "main-data-ss"                    },                    {                        "title": "Security",                        "path": "data/security.html",                        "id": "main-data-security"                    },                    {                        "title": "Adding external data sources",                        "path": "data/adding-data-sources.html",                        "id": "main-data-external"                    }                ]            },            {                "title": "Authentication",                "path": "authentication.html",                "id": "main-auth",                "sections": [{                        "title": "Default Mechanism",                        "path": "authentication/default.html",                        "id": "main-auth-default"                    },                    {                        "title": "Custom Authentication",                        "path": "authentication/custom.html",                        "id": "main-auth-custom"                    },                    {                        "title": "Custom Session Management",                        "path": "authentication/custom-session-management.html",                        "id": "main-auth-csm"                    }                ]            },            {                "id": "main-http",                "title": "HTTP",                "path": "http.html"            },            {                "id": "main-workers",                "title": "Workers",                "path": "threads.html",                "sections": [{		              "title": "System Worker",		              "id": "main-workers-system",		              "path": "threads/system-worker.html"		            },		            {		              "title": "Shared Worker",		              "id": "main-workers-shared",		              "path": "threads/shared-worker.html"		            },		            {		              "title": "Node Worker",		              "id": "main-workers-node",		              "path": "threads/node-worker.html"		            }            	]            },            {                "id": "main-admin",                "title": "Administration",                "path": "administration.html"            },            {                "id": "main-misc",                "title": "Miscellaneous",                "path": "miscellaneous.html"            }        ]    }};

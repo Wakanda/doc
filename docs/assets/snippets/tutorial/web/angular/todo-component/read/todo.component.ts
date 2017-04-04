402c158ac664cb1c129f5462b406ba3dad21f32a
Hamza HIKMAT <hamzahik@gmail.com>
2017-04-04 19:14:10 +0200

    build

--- docs/assets/snippets/tutorial/web/angular/todo-component/read/todo.component.ts
+++ docs/assets/snippets/tutorial/web/angular/todo-component/read/todo.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Wakanda } from '../wakanda.service';
-import {  Router } from '@angular/router';
 
 
 @Component({
@@ -11,23 +10,20 @@ import {  Router } from '@angular/router';
 })
 export class TodoComponent implements OnInit {
   
-  todos ;
-  newTodoText :string = "";
+    todos = [];
   
-  constructor(public wakanda: Wakanda,public router:Router) { 
-  	this.todos = []
-  }
-
+    constructor(public wakanda: Wakanda) { 
+        
+    }
 	
-  ngOnInit() {}
+    ngOnInit() {}
 
-  getTodos() {
-	    this.wakanda.catalog.then(ds => {
+    getTodos() {
+	    this.wakanda.getCatalog().then(ds => {
 	        ds['Todo'].query().then(collection => {
 	        	this.todos = collection.entities;
 	        });
 	    });
-	}
-
+    }
 
 }

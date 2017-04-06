7a0a66b3d74b87afdd04a010bcfa34ae32c2a9ca
Hamza HIKMAT <hamzahik@gmail.com>
2017-04-06 16:14:02 +0200

    update SharedWorker and SystemWorker snippets

--- web/src/assets/snippets/threads/system-worker/asynchronous.js
+++ web/src/assets/snippets/threads/system-worker/asynchronous.js
@@ -1,13 +1,22 @@
 var workerProxy = new SystemWorker( 'sh -c ls -la /Users/<user>/Desktop' );
+
 workerProxy.onerror = function ( event ) {      
     console.log( event.type +': '+ event.data.toString() );
-}
+};
+
 workerProxy.onmessage = function ( message ) {      
     console.log( event.type +': '+ event.data.toString() );
-}
+};
+
 workerProxy.onterminated = function ( event ) {
     console.log( event.type +': with exitStatus:'+ event.exitStatus );
-}
-// For testing purpose, wait for the worker to end. This makes it very similar to SystemWorker.exec().
-// In real application, keep working with callbacks and do not pause the current thread
+};
+
+/**
+ * For testing purposes, wait for the process to exit. This makes it very similar to SystemWorker.exec().
+ * If you are using a SystemWorker from a SharedWorker you won't need to use `wait` but if your call
+ * is trigerred from an HTTP request you'll need to wait for the process to exit otherwise your code
+ * might not get any output.
+ **/
+
 workerProxy.wait();

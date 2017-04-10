85e20251d7d72fcf272cecdb59b911b15229e0fa
Hamza HIKMAT <hamzahik@gmail.com>
2017-04-10 17:44:31 +0200

    tuto : update snippet for angular todo

--- web/src/assets/snippets/tutorial/web/angular/todo-component/create/todo.component.ts
+++ web/src/assets/snippets/tutorial/web/angular/todo-component/create/todo.component.ts
@@ -1,17 +1,50 @@
-newTodo() {
-    this.wakanda.catalog.then(ds => {
-        let todo = ds['Todo'].create({
-            label: this.newTodoText,
-            completed: false
-        });
-        todo.save().then(() => {
-        	alert('saved')
-            this.todos.push({
-                ID: todo['ID'],
-                label: this.newTodoText,
-                completed: false
-            });
-        this.newTodoText = ""; //clear the input 
-        });
-    });
+import { Component, OnInit } from '@angular/core';
+import { Wakanda } from '../wakanda.service';
+
+
+@Component({
+  selector: 'app-todo',
+  providers: [Wakanda],
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  
+    todos = [];
+  
+    constructor(public wakanda: Wakanda) { 
+        
+    }
+	
+    ngOnInit() {}
+
+    getTodos() {
+	    this.wakanda.getCatalog().then(ds => {
+	        ds['Todo'].query()
+	        .then(collection => {
+	        	this.todos = collection.entities;
+	        });
+	    });
+    }
+    
+    newTodo() {
+	    this.wakanda.catalog.then(ds => {
+	        let todo = ds['Todo'].create({
+	            label: this.newTodoText,
+	            completed: false
+	        });
+	        
+	        todo.save()
+	        .then(() => {
+	        	alert('saved')
+	            this.todos.push({
+	                ID: todo['ID'],
+	                label: this.newTodoText,
+	                completed: false
+	            });
+	            
+	        	this.newTodoText = ""; //clear the input 
+	        });
+	    });
+	}
 }

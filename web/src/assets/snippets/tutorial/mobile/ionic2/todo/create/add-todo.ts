ea89a0152fc0c50d121f3e0979c097c209717a80
Hamza HIKMAT <hamzahik@gmail.com>
2017-05-09 11:12:17 +0200

    add newTodoText class member

--- web/src/assets/snippets/tutorial/mobile/ionic2/todo/create/add-todo.ts
+++ web/src/assets/snippets/tutorial/mobile/ionic2/todo/create/add-todo.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams,ViewController } from 'ionic-angular';
 import { Wakanda } from '../../app/wakanda.service';
+
 /*
   Generated class for the AddTodo page.
 
@@ -8,36 +9,38 @@ import { Wakanda } from '../../app/wakanda.service';
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-add-todo',
-  templateUrl: 'add-todo.html',
-  providers: [Wakanda]
+    selector: 'page-add-todo',
+    templateUrl: 'add-todo.html',
+    providers: [Wakanda]
 })
 export class AddTodoPage {
-  newTodoText : string = "";
-  constructor(public navCtrl: NavController,
-			  public navParams: NavParams, 
-			  public wakanda: Wakanda,
-			  public view: ViewController) {}
+	
+    newTodoText: string = "";
+    
+    constructor(public navCtrl: NavController,
+        public navParams: NavParams,
+        public wakanda: Wakanda,
+        public view: ViewController) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddTodoPage');
-  }
-  
-saveTodo() {
-    this.wakanda.catalog.then(ds => {
-        let todo = ds['Todo'].create({
-            label: this.newTodoText,
-            completed: false
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad AddTodoPage');
+    }
+
+    saveTodo() {
+        this.wakanda.catalog.then(ds => {
+            let todo = ds['Todo'].create({
+                label: this.newTodoText,
+                completed: false
+            });
+            todo.save().then(() => {
+                this.view.dismiss(todo)
+            })
         });
-        todo.save().then(() => {
-            this.view.dismiss(todo)
-        })
-    });
 
-}
+    }
 
-close() {
-    this.view.dismiss();
-}
+    close() {
+        this.view.dismiss();
+    }
 
 }

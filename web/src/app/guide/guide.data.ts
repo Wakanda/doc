3bf36dfdaa7c1ba655ee203d855cb4af67d29639
Yann <yann.jego@wakanda.io>
2017-07-26 14:40:52 +0200

    convert weird line endings

--- web/src/app/guide/guide.data.ts
+++ web/src/app/guide/guide.data.ts
@@ -1,1 +1,217 @@
-export default {    tree: {        "id": "main",        "title": "Home",        "root": true,        "path": "home.html",        "sections": [{            "id": "main-overview",            "title": "Concepts",            "path": "overview.html",            "sections": [{                "id": "main-overview-solution",                "title": "Solution",                "path": "overview/solution.html"            },            {                "id": "main-overview-project",                "title": "Project",                "path": "overview/project.html"            },            {                "id": "main-overview-bootstrap",                "title": "Backend - Bootstrap",                "path": "overview/bootstrap.html"            },            {                "id": "main-overview-modules",                "title": "Backend - Modules",                "path": "overview/modules.html"            },            {                "id": "main-overview-services",                "title": "Backend - Services",                "path": "overview/services.html"            }            ]        },        {            "title": "Data Integration",            "path": "data.html",            "id": "main-data",            "sections": [                {                    "title": "Basics",                    "path": "data/basics.html",                    "id": "main-data-basics"                },                {                    "title": "Client side",                    "path": "data/client-side.html",                    "id": "main-data-cs",                    "sections": [{                        "title": "Get started",                        "id": "main-data-cs-instance",                        "path": "data/client-side/javascript-client-instance.html"                    },                    {                        "title": "Retrieve entities",                        "id": "main-data-cs-retrieve",                        "path": "data/client-side/javascript-client-retrieve-entities.html"                    },                    {                        "title": "Handle entities",                        "id": "main-data-cs-manipulate",                        "path": "data/client-side/javascript-client-manipulate-entity.html"                    },                    {                        "title": "Handle related entities",                        "id": "main-data-cs-related",                        "path": "data/client-side/javascript-client-related-entities.html"                    },                    {                        "title": "Handle collections",                        "id": "main-data-cs-helpers",                        "path": "data/client-side/javascript-client-collection-helpers.html"                    },                    {                        "title": "Images and Blobs",                        "id": "main-data-cs-upload",                        "path": "data/client-side/javascript-client-upload.html"                    },                    {                        "title": "Server-side methods",                        "id": "main-data-cs-servermethods",                        "path": "data/client-side/javascript-client-server-methods.html"                    },                    {                        "title": "User authentication",                        "id": "main-data-cs-directorymethods",                        "path": "data/client-side/javascript-client-directory-methods.html"                    }                    ]                },                {                    "title": "Server Side",                    "path": "data/server-side.html",                    "id": "main-data-ss"                },                {                    "title": "Security",                    "path": "data/security.html",                    "id": "main-data-security"                },                {                    "title": "Handling data sources",                    "path": "data/adding-data-sources.html",                    "id": "main-data-external",                    "sections": [                        {                            "title": "Model Designer",                            "id": "main-data-external-modeldesigner",                            "path": "data/adding-data-sources/modeldesigner.html"                        },                        {                            "title": "Enrich Model",                            "id": "main-data-external-enrich",                            "path": "data/adding-data-sources/enrich.html"                        },                        {                            "title": "MYSQL",                            "id": "main-data-external-mysql",                            "path": "data/adding-data-sources/mysql.html"                        },                        {                            "title": "ODBC",                            "id": "main-data-external-odbc",                            "path": "data/adding-data-sources/odbc.html"                        },                        {                            "title": "Virtual",                            "id": "main-data-external-virtual",                            "path": "data/adding-data-sources/virtual.html"                        }                    ]                }            ]        },        {            "title": "Authentication",            "path": "authentication.html",            "id": "main-auth",            "sections": [{                "title": "Default Mechanism",                "path": "authentication/default.html",                "id": "main-auth-default"            },            {                "title": "Custom Authentication",                "path": "authentication/custom.html",                "id": "main-auth-custom"            },            {                "title": "Custom Session Management",                "path": "authentication/custom-session-management.html",                "id": "main-auth-csm"            }            ]        },        {            "id": "main-http",            "title": "HTTP",            "path": "http.html"        },        {            "id": "main-workers",            "title": "Workers",            "path": "threads.html",            "sections": [{                "title": "System Worker",                "id": "main-workers-system",                "path": "threads/system-worker.html"            },            {                "title": "Shared Worker",                "id": "main-workers-shared",                "path": "threads/shared-worker.html"            },            {                "title": "Node Worker",                "id": "main-workers-node",                "path": "threads/node-worker.html"            },            {                "title": "Threadify",                "id": "main-workers-threadify",                "path": "threads/threadify.html"            }            ]        },        {            "id": "main-test",            "title": "Testing",            "path": "testing.html"        },        {            "id": "main-admin",            "title": "Administration",            "path": "administration.html"        },        {            "id": "main-misc",            "title": "Miscellaneous",            "path": "miscellaneous.html"        },        {            "id": "main-migrate1to2",            "title": "Migration from v1 to v2",            "path": "migration-from-v1-to-v2.html"        },        {            "id": "main-requirements",            "title": "Requirements",            "path": "requirements.html"        }        ]    }};
+export default {
+    tree: {
+        "id": "main",
+        "title": "Home",
+        "root": true,
+        "path": "home.html",
+        "sections": [{
+            "id": "main-overview",
+            "title": "Concepts",
+            "path": "overview.html",
+            "sections": [{
+                "id": "main-overview-solution",
+                "title": "Solution",
+                "path": "overview/solution.html"
+            },
+            {
+                "id": "main-overview-project",
+                "title": "Project",
+                "path": "overview/project.html"
+            },
+            {
+                "id": "main-overview-bootstrap",
+                "title": "Backend - Bootstrap",
+                "path": "overview/bootstrap.html"
+            },
+            {
+                "id": "main-overview-modules",
+                "title": "Backend - Modules",
+                "path": "overview/modules.html"
+            },
+            {
+                "id": "main-overview-services",
+                "title": "Backend - Services",
+                "path": "overview/services.html"
+            }
+            ]
+        },
+        {
+            "title": "Data Integration",
+            "path": "data.html",
+            "id": "main-data",
+            "sections": [
+                {
+                    "title": "Basics",
+                    "path": "data/basics.html",
+                    "id": "main-data-basics"
+                },
+                {
+                    "title": "Client side",
+                    "path": "data/client-side.html",
+                    "id": "main-data-cs",
+                    "sections": [{
+                        "title": "Get started",
+                        "id": "main-data-cs-instance",
+                        "path": "data/client-side/javascript-client-instance.html"
+                    },
+                    {
+                        "title": "Retrieve entities",
+                        "id": "main-data-cs-retrieve",
+                        "path": "data/client-side/javascript-client-retrieve-entities.html"
+                    },
+                    {
+                        "title": "Handle entities",
+                        "id": "main-data-cs-manipulate",
+                        "path": "data/client-side/javascript-client-manipulate-entity.html"
+                    },
+                    {
+                        "title": "Handle related entities",
+                        "id": "main-data-cs-related",
+                        "path": "data/client-side/javascript-client-related-entities.html"
+                    },
+                    {
+                        "title": "Handle collections",
+                        "id": "main-data-cs-helpers",
+                        "path": "data/client-side/javascript-client-collection-helpers.html"
+                    },
+                    {
+                        "title": "Images and Blobs",
+                        "id": "main-data-cs-upload",
+                        "path": "data/client-side/javascript-client-upload.html"
+                    },
+                    {
+                        "title": "Server-side methods",
+                        "id": "main-data-cs-servermethods",
+                        "path": "data/client-side/javascript-client-server-methods.html"
+                    },
+                    {
+                        "title": "User authentication",
+                        "id": "main-data-cs-directorymethods",
+                        "path": "data/client-side/javascript-client-directory-methods.html"
+                    }
+                    ]
+                },
+                {
+                    "title": "Server Side",
+                    "path": "data/server-side.html",
+                    "id": "main-data-ss"
+                },
+                {
+                    "title": "Security",
+                    "path": "data/security.html",
+                    "id": "main-data-security"
+                },
+                {
+                    "title": "Handling data sources",
+                    "path": "data/adding-data-sources.html",
+                    "id": "main-data-external",
+                    "sections": [
+                        {
+                            "title": "Model Designer",
+                            "id": "main-data-external-modeldesigner",
+                            "path": "data/adding-data-sources/modeldesigner.html"
+                        },
+                        {
+                            "title": "Enrich Model",
+                            "id": "main-data-external-enrich",
+                            "path": "data/adding-data-sources/enrich.html"
+                        },
+                        {
+                            "title": "MYSQL",
+                            "id": "main-data-external-mysql",
+                            "path": "data/adding-data-sources/mysql.html"
+                        },
+                        {
+                            "title": "ODBC",
+                            "id": "main-data-external-odbc",
+                            "path": "data/adding-data-sources/odbc.html"
+                        },
+                        {
+                            "title": "Virtual",
+                            "id": "main-data-external-virtual",
+                            "path": "data/adding-data-sources/virtual.html"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "title": "Authentication",
+            "path": "authentication.html",
+            "id": "main-auth",
+            "sections": [{
+                "title": "Default Mechanism",
+                "path": "authentication/default.html",
+                "id": "main-auth-default"
+            },
+            {
+                "title": "Custom Authentication",
+                "path": "authentication/custom.html",
+                "id": "main-auth-custom"
+            },
+            {
+                "title": "Custom Session Management",
+                "path": "authentication/custom-session-management.html",
+                "id": "main-auth-csm"
+            }
+            ]
+        },
+        {
+            "id": "main-http",
+            "title": "HTTP",
+            "path": "http.html"
+        },
+        {
+            "id": "main-workers",
+            "title": "Workers",
+            "path": "threads.html",
+            "sections": [{
+                "title": "System Worker",
+                "id": "main-workers-system",
+                "path": "threads/system-worker.html"
+            },
+            {
+                "title": "Shared Worker",
+                "id": "main-workers-shared",
+                "path": "threads/shared-worker.html"
+            },
+            {
+                "title": "Node Worker",
+                "id": "main-workers-node",
+                "path": "threads/node-worker.html"
+            },
+            {
+                "title": "Threadify",
+                "id": "main-workers-threadify",
+                "path": "threads/threadify.html"
+            }
+            ]
+        },
+        {
+            "id": "main-test",
+            "title": "Testing",
+            "path": "testing.html"
+        },
+        {
+            "id": "main-admin",
+            "title": "Administration",
+            "path": "administration.html"
+        },
+        {
+            "id": "main-misc",
+            "title": "Miscellaneous",
+            "path": "miscellaneous.html"
+        },
+        {
+            "id": "main-migrate1to2",
+            "title": "Migration from v1 to v2",
+            "path": "migration-from-v1-to-v2.html"
+        },
+        {
+            "id": "main-requirements",
+            "title": "Requirements",
+            "path": "requirements.html"
+        }
+        ]
+    }
+};
